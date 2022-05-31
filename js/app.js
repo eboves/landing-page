d3d495c95de8758bc0a4369fda84547bb45cd7ab
Elvis Boves <elvisboves@Elviss-MacBook-Pro.local>
2022-05-31 19:20:34 -0400

    change color of active section

--- js/app.js
+++ js/app.js
@@ -13,6 +13,12 @@
  *
  */
 
+// TODO:
+// add dynamically li
+// add class 1st child
+
+// remove your-active-class
+
 // Variables
 let nav_item_list = [
   "Section 1",
@@ -26,15 +32,33 @@ let nav_id_list = ["section1", "section2", "section3", "section4", "section5"];
 const nav_bar_list = document.getElementById("navbar__list");
 const btn_nav_el = document.querySelector(".btn-nav");
 const header_el = document.querySelector(".page__header");
-
 const sections = document.querySelectorAll("section");
-const nav_links = document.querySelectorAll(".links");
+const nav_list = document.querySelectorAll("li");
 
 //this code creates the dynamic ul with a class as well as the href
 //with the id of each section
 for (let i = 0; i < nav_item_list.length; i++) {
-  nav_bar_list.innerHTML += `<li class="menu__link ${nav_id_list[i]}"><a class="links" href="#${nav_id_list[i]}">${nav_item_list[i]}</a></li>`;
+  nav_bar_list.innerHTML += `<li class="menu__link ${nav_id_list[i]} active"><a class="links" href="#${nav_id_list[i]}">${nav_item_list[i]}</a></li>`;
 }
+
+window.addEventListener("scroll", function () {
+  let current_section = "";
+  sections.forEach((section) => {
+    const section_top = section.offsetTop;
+
+    section.classList.remove("your-active-class");
+    if (this.scrollY >= section_top - 250) {
+      current_section = section.getAttribute("id");
+      section.classList.add("your-active-class");
+    }
+  });
+  document.querySelectorAll("li").forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current_section)) {
+      li.classList.add("active");
+    }
+  });
+});
 
 // This code select all the a tags and make a smooth scrolling motion
 // when changing from section to section
@@ -47,30 +71,7 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-nav_links.forEach((link) => {
-  link.addEventListener("click", function () {
-    console.log("i was clicked");
-  });
-});
-
-window.addEventListener("scroll", function () {
-  // let current = "";
-  // sections.forEach((section) => {
-  //   const section_top = section.offsetTop;
-  //   const section_height = section.clientHeight;
-  //   if (this.pageYOffset >= section_top - section_height / 3) {
-  //     current = section.getAttribute("id");
-  //   }
-  // });
-  // sections.forEach((section) => {
-  //   section.classList.remove("your-active-class");
-  //   if (section.classList.contains(current)) {
-  //     section.classList.add("your-active-class");
-  //   }
-  // });
-});
-
-// open and close the nav bar on smaller divices
+// This code opens/closes the nav bar on smaller divices
 btn_nav_el.addEventListener("click", function () {
   header_el.classList.toggle("nav-open");
 });
