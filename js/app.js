dd87bff0ae4f664e43b328fcebea2c69e8ff975b
Elvis Boves <elvisboves@Elviss-MacBook-Pro.local>
2022-06-05 19:38:21 -0400

    type: feat
    body: Add button that takes you to the top of the page when clicked
    
    footer: #8

--- js/app.js
+++ js/app.js
@@ -14,18 +14,17 @@
  */
 ///////////////////////////////////////////////////
 // TODO:
-// add dynamically li
-// add class 1st child
 
 // remove your-active-class
 
 ////////////////////////////////////////////////
 
 const nav_bar_list = document.getElementById("navbar__list");
-const btn_nav_el = document.querySelector(".btn-nav");
 const header_el = document.querySelector(".page__header");
+const btn_nav_el = document.querySelector(".btn-nav");
 const sections = document.querySelectorAll("section");
 const nav_list = document.querySelectorAll("li");
+const btn_top = document.querySelector(".btn");
 
 //this code creates the dynamic ul with a class as well as the href
 //with the id of each section
@@ -49,6 +48,7 @@ function nav_highlighting(current_section) {
     }
   });
 }
+
 // This function add or remove the your-active-class
 // if the section is on the viewport. If the top of the section,
 // is the top of the section is less than the windows innerHeight,
@@ -68,28 +68,55 @@ function active_class() {
       section.classList.remove("your-active-class");
     }
   });
+  return current_section;
 }
 
-// function smooth_scroll(){}
-window.addEventListener("scroll", function () {
-  let current_section = "";
-
-  //calling the active_class function
-  active_class();
-  //calling the nav_highlighting function
-  nav_highlighting(current_section);
-});
-
-// This code select all the a tags and make a smooth scrolling motion
-// when changing from section to section
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
+// This function handle the button that takes you to the top
+// of the page when clicked and with the scrolling animation
+function to_top() {
+  btn_top.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      left: 0,
       behavior: "smooth",
     });
-    header_el.classList.remove("nav-open");
   });
+}
+// calling the to_top function
+to_top();
+
+function smooth_scrolling(anchor) {
+  document.querySelector(anchor.getAttribute("href")).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+// This function select all the a tags and make a smooth scrolling motion
+// when you click on the section tag on the nav
+function section_click() {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      // document.querySelector(this.getAttribute("href")).scrollIntoView({
+      //   behavior: "smooth",
+      // });
+      smooth_scrolling(anchor);
+      header_el.classList.remove("nav-open");
+    });
+  });
+}
+// calling the section_click function
+section_click();
+
+// This call to the window object when scrolling calls the active_class
+// and the nav_highlighting functions
+window.addEventListener("scroll", function () {
+  // holding the value return when calling active class to pass it as
+  // argument to nav_highlighting
+  let current_section = active_class();
+
+  //calling the nav_highlighting function
+  nav_highlighting(current_section);
 });
 
 // This code opens/closes the nav bar on smaller divices
